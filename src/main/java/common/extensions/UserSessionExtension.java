f2012c43c45e4151826631a40f62938acf765965
antoha <antonsergeevichk@gmail.com>
2026-06-02 23:14:16 +0300

    Оптимизировал тесты. Убрал хардкод.

--- src/main/java/common/extensions/UserSessionExtension.java
+++ src/main/java/common/extensions/UserSessionExtension.java
@@ -2,13 +2,10 @@ package common.extensions;
 
 import api.configs.Config;
 import api.models.CustomerModel;
-import com.codeborne.selenide.Selenide;
 import common.annotations.UserSession;
 import common.storage.SessionStorage;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import ui.pages.LoginPage;
-
 import java.util.List;
 
 public class UserSessionExtension implements BeforeEachCallback {
@@ -16,10 +13,8 @@ public class UserSessionExtension implements BeforeEachCallback {
     public void beforeEach(ExtensionContext extensionContext) throws Exception {
         UserSession annotation = extensionContext.getRequiredTestMethod().getAnnotation(UserSession.class);
         if (annotation != null) {
-
-            CustomerModel user;
             try {
-                user = SessionStorage.getUser();
+                SessionStorage.getUser();
             } catch (IndexOutOfBoundsException | IllegalStateException e) {
                 String defaultLogin = Config.getProperty("defaultApiUser");
                 String defaultPassword = Config.getProperty("defaultApiPassword");
@@ -27,17 +22,13 @@ public class UserSessionExtension implements BeforeEachCallback {
                 if (defaultLogin == null) defaultLogin = "kate1998";
                 if (defaultPassword == null) defaultPassword = "verysTRongPassword33$";
 
-                user = CustomerModel.builder()
+                CustomerModel user = CustomerModel.builder()
                         .username(defaultLogin)
                         .password(defaultPassword)
                         .build();
 
                 SessionStorage.addUsers(List.of(user));
             }
-
-            Selenide.open("/");
-
-            new LoginPage().authAsUser(user.getUsername(),user.getPassword());
         }
     }
 }
